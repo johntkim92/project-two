d3543ca2b462fb9976cf72bfe17a6262a6b63f4b
John Kim <johntkim92@gmail.com>
2015-10-09 10:42:25 -0400

    connecting to mongoose

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express'),
     PORT    = process.env.PORT || 5432,
-    server  = express();
+    server  = express(),
+    MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+    dbname = "please_change_this",
+    mongoose = require('mongoose');
 
 server.get('/test', function (req, res) {
   res.write("Welcome to my amazing app");
   res.end();
 });
 
+mongoose.connect(MONGOURI);
 server.listen(PORT, function () {
   console.log("SERVER IS UP ON PORT:", PORT);
 });
-    
