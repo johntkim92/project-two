7b697563d8398a3318bceb6f5d4ff2b6180fb040
John Kim <johntkim92@gmail.com>
2015-10-12 18:16:25 -0400

    topic schema and files added

---
+++ models/topic.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var topicSchema = new Schema({
+  title: { type: String, required: true},
+  author: String,
+  body: String,
+  comments: [{ content: String}]
+  });
+});
+
+var Topic = mongoose.model("Topic", topicSchema);
+
+module.exports = Topic;
