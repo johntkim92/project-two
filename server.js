ade755d8457ff9b5df111cd04fb261c18f895f76
John Kim <johntkim92@gmail.com>
2015-10-12 01:35:07 -0400

    adding user login

--- server.js
+++ server.js
@@ -14,13 +14,26 @@ var express = require('express'),
 var topicSchema = new Schema ({
   title: String,
   author: String,
+  body: String,
   comments: [{ content: String}]
 });
 
+var userSchema = Schema({
+  username: { type: String, required: true},
+  password: { type: String, required: true}
+});
+
+var User = mongoose.model('user', userSchema);
 var Topic = mongoose.model('topic', topicSchema);
 
 server.set('views', './views');
 server.set('view engine', 'ejs');
+
+server.use(session({
+  secret: "jaxwillbethelastonestanding",
+  resave: true,
+  saveUninitialized: false
+}));
 
 server.use(express.static('./public'));
 server.use(bodyParser.urlencoded({
@@ -32,6 +45,25 @@ server.get('/test', function (req, res) {
   res.write("Welcome to my amazing app");
   res.end();
 });
+
+server.get('/new', function (req, res) {
+  res.render('users/new-user');
+});
+
+server.post('/', function (req, res) {
+  var newUser = User(req.body.user);
+
+  newUser.save(function (err, user) {
+    res.redirect(301, "/users/" + user._id)
+  });
+});
+
+server.get('/:id', function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    console.log(user);
+  });
+});
+
 
 mongoose.connect(MONGOURI + "/" + DBNAME);
 server.listen(PORT, function () {
